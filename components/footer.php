0a554c67ed1d6b94e407b20af474bd6fbd1c4fe5
munachimso <victorycollins31@gmail.com>
2024-12-21 06:59:11 +0100

    ⚡️ created components

---
+++ components/footer.php
@@ -0,0 +1,12 @@
+   <footer class="flex flex-wrap justify-between items-center gap-4 text-xs">
+       <button class="flex items-center gap-2 bg-zinc-300 bg-opacity-50 px-2.5 py-1.5 rounded-sm">
+           <img
+               src="img/whatsapp.jpg"
+               alt=""
+               width="18"
+               height="18"
+               aria-hidden="true" />
+           Contact Support
+       </button>
+       <p>&copy; 2024 Sentinelock. All rights reserved!</p>
+   </footer>
---
+++ components/welcome.php
@@ -0,0 +1,19 @@
+<header class="flex items-center gap-3 mb-8">
+    <img
+        src="img/sentinel.jpg"
+        alt="Sentinel Logo"
+        width="31"
+        height="46" />
+    <h1
+        id="welcome-heading"
+        class="text-3xl font-black">
+        Sentinel
+    </h1>
+</header>
+
+<div class="welcome-text mb-10">
+    <h2 class="text-4xl font-medium w-[60%] leading-snug">
+        Welcome to
+        <strong>Sentinel Cashback Program</strong>
+    </h2>
+</div>
